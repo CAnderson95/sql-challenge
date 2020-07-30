3828b3e75bf84c3a391701aef569eebba5e3941f
CAnderson95 <christian.c.anderson@outlook.com>
2020-07-30 15:17:13 -0400

    First Commit

---
+++ Homework.sql
@@ -0,0 +1,109 @@
+-- Drop Tables if Existing
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Import CSV Files Into Corresponding SQL Table
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR NOT NULL,
+    "dept_name" VARCHAR NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR NOT NULL,
+    "emp_no" INT NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT NOT NULL,
+    "emp_title_id" VARCHAR NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+	"sex" VARCHAR NOT NULL,
+    "hire_date" DATE NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT NOT NULL,
+    "salary" INT NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR NOT NULL,
+	"title" VARCHAR NOT NULL
+);
+
+--Display Employee Details
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+--Display Employees hired in 1986
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+
+--List each Department Manager
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+-- List Employee Department
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+--List all employees with name Hercules last initial B
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List Sales Department Employees
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+--List Sales and Development Departments Employees
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' 
+OR departments.dept_name = 'Development';
+
+--List Employee Last Name Frequency in Descending Order
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
---
+++ creds.py
@@ -0,0 +1,6 @@
+creds = dict(driver='{ODBC Driver 13 for SQL Server}',
+             server='localhost',
+             port=5432,
+             database='employee_db',
+             user='postgres',
+             passwd='FreshmeN1')
